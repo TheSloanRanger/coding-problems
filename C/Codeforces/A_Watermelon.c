152afccad31be5d93393d6c20a533adaaa33f60d
Ben Sloan <mrbensloan@gmail.com>
2026-09-05 00:14:08 +0100

    Restructuring repo and did first codeforces problem in C

---
+++ C/Codeforces/A_Watermelon.c
@@ -0,0 +1,15 @@
+// Author: Ben Sloan
+
+#include <stdio.h>
+
+int main() {
+
+    int x;
+    
+    scanf("%d", &x);
+
+    if (x % 2 == 0 && x != 2) {
+        printf("YES");
+    } else printf("NO"); 
+
+}
